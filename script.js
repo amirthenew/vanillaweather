75aa1e1827e65365b1f0df0d7c0711a9aa7d34bd
amirthenew <ah.abshari@hotmail.com>
2023-02-04 23:49:46 +0330

    2 city is now in app london and tehran

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 let locationTag = document.querySelector('.location')
 let tempTag = document.querySelector('.temp')
 let conditionTag = document.querySelector('.condition')
+let iconTag = document.querySelector('.icon')
+let londonTag = document.querySelector('.london')
+let theranTag = document.querySelector('.tehran')
 
-const URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tehran?unitGroup=metric&include=current%2Cdays&key=6BWV8R97HT8YYDU9CMJMLNDST&contentType=json'
-const test = function(){
-    fetch(URL)
+
+let apiTehran = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tehran?unitGroup=metric&key=6BWV8R97HT8YYDU9CMJMLNDST&contentType=json'
+let apiLondon = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=metric&key=6BWV8R97HT8YYDU9CMJMLNDST&contentType=json'
+
+
+
+const showLondon = function(){
+
+    fetch(apiLondon)
+    .then((res)=>res.json())
+.then (
+    (data)=>{
+        console.log(data)
+        let location = data.resolvedAddress
+        let temp = data.currentConditions.temp
+        let condition = data.currentConditions.conditions
+        let icon = data.currentConditions.icon
+        locationTag.innerHTML = location
+        tempTag.innerHTML = temp
+        conditionTag.innerHTML = condition
+        iconTag.innerHTML = icon
+
+        console.log(location);
+        console.log(temp);
+        console.log(condition);
+    
+    
+    }
+    
+    )
+}
+
+const showTehran = function(){
+    fetch(apiTehran)
 .then((res)=>res.json())
 .then (
     (data)=>{
@@ -12,9 +46,11 @@ const test = function(){
         let location = data.resolvedAddress
         let temp = data.currentConditions.temp
         let condition = data.currentConditions.conditions
+        let icon = data.currentConditions.icon
         locationTag.innerHTML = location
         tempTag.innerHTML = temp
         conditionTag.innerHTML = condition
+        iconTag.innerHTML = icon
 
         console.log(location);
         console.log(temp);
@@ -26,5 +62,5 @@ const test = function(){
     )
 
 }
-
-test()
+londonTag.addEventListener('click',showLondon)
+theranTag.addEventListener('click',showTehran)
