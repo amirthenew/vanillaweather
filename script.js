c5f846d2722afc91e63a2be5b6cc44efef912f0d
amirthenew <ah.abshari@hotmail.com>
2023-02-04 23:06:41 +0330

    start

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 console.log('hello world');
 
 
+const URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tehran?unitGroup=metric&include=current&key=6BWV8R97HT8YYDU9CMJMLNDST&contentType=json'
 const test = function(){
-    fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tehran?unitGroup=metric&include=current&key=6BWV8R97HT8YYDU9CMJMLNDST&contentType=json')
+    fetch(URL)
 .then((res)=>res.json())
 .then (data=>console.log(data))
+
+let city = data.resolvedAddress
+
 }
 
 test()
