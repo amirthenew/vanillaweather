2c4f9d234955fb6ce588fd12d4dc4fcd3eea19c4
amirthenew <ah.abshari@hotmail.com>
2023-02-04 23:26:05 +0330

    styling + api is ok

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-console.log('hello world');
+let locationTag = document.querySelector('.location')
+let tempTag = document.querySelector('.temp')
+let conditionTag = document.querySelector('.condition')
 
-
-const URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tehran?unitGroup=metric&include=current&key=6BWV8R97HT8YYDU9CMJMLNDST&contentType=json'
+const URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tehran?unitGroup=metric&include=current%2Cdays&key=6BWV8R97HT8YYDU9CMJMLNDST&contentType=json'
 const test = function(){
     fetch(URL)
 .then((res)=>res.json())
-.then (data=>console.log(data))
+.then (
+    (data)=>{
+        console.log(data)
+        let location = data.resolvedAddress
+        let temp = data.currentConditions.temp
+        let condition = data.currentConditions.conditions
+        locationTag.innerHTML = location
+        tempTag.innerHTML = temp
+        conditionTag.innerHTML = condition
 
-let city = data.resolvedAddress
+        console.log(location);
+        console.log(temp);
+        console.log(condition);
+    
+    
+    }
+    
+    )
 
 }
 
